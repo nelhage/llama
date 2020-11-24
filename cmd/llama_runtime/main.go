843171ba82531274bdd30a4577135138dbc4d318
Nelson Elhage <nelhage@nelhage.com>
2020-11-24 12:29:16 -0800

    initial work towards args

--- cmd/llama_runtime/main.go
+++ cmd/llama_runtime/main.go
@@ -4,15 +4,34 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"os/exec"
 	"path"
-	"strings"
+
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/nelhage/llama/protocol"
+	"github.com/nelhage/llama/store"
+	"github.com/nelhage/llama/store/s3store"
 )
+
+func initStore() (store.Store, error) {
+	session, err := session.NewSession()
+	if err != nil {
+		return nil, err
+	}
+	bucket := os.Getenv("LLAMA_BUCKET")
+	if bucket == "" {
+		return nil, errors.New("Could not read llama s3 bucket from LLAMA_BUCKET")
+	}
+	s3 := s3store.FromSession(session, bucket)
+	return s3, nil
+}
 
 func main() {
 	runtimeURI := os.Getenv("AWS_LAMBDA_RUNTIME_API")
@@ -22,6 +41,16 @@ func main() {
 
 	client := http.Client{}
 	ctx := context.Background()
+
+	store, err := initStore()
+	if err != nil {
+		payload, _ := json.Marshal(struct {
+			Error string `json:"error"`
+		}{fmt.Sprintf("Unable to initialize store: %s", err.Error())})
+		req, _ := http.NewRequestWithContext(ctx, "POST", fmt.Sprintf("http://%s/2018-06-01/runtime/init/error", runtimeURI), bytes.NewReader(payload))
+		client.Do(req)
+		os.Exit(1)
+	}
 
 	for {
 		req, err := http.NewRequestWithContext(ctx, "GET", fmt.Sprintf("http://%s/2018-06-01/runtime/invocation/next", runtimeURI), nil)
@@ -34,7 +63,7 @@ func main() {
 		}
 		reqId := job.Header.Get("Lambda-Runtime-Aws-Request-Id")
 
-		resp, err := runOne(job)
+		resp, err := runOne(ctx, store, job)
 		var payload []byte
 		if err == nil {
 			payload, err = json.Marshal(resp)
@@ -59,39 +88,57 @@ func main() {
 	}
 }
 
-type InvocationSpec struct {
-	Args  []string `json:"args"`
-	Stdin string   `json:"stdin"`
+type ParsedJob struct {
+	Temp    string
+	Exe     string
+	Root    string
+	Args    []string
+	Stdin   []byte
+	Outputs map[string]string
 }
 
-type InvocationResponse struct {
-	ExitStatus int    `json:"status"`
-	Stdout     string `json:"stdout"`
-	Stderr     string `json:"stderr"`
+func (p *ParsedJob) Cleanup() error {
+	if p.Temp != "" {
+		return os.RemoveAll(p.Temp)
+	}
+	return nil
 }
 
-func runOne(job *http.Response) (interface{}, error) {
-	handler := os.Getenv("_HANDLER")
-	root := os.Getenv("LAMBDA_TASK_ROOT")
+func (p *ParsedJob) EnsureTemp() (string, error) {
+	if p.Temp == "" {
+		var err error
+		p.Temp, err = ioutil.TempDir("", "llama.*")
+		if err != nil {
+			return "", err
+		}
+	}
+	return p.Temp, nil
+}
 
-	var spec InvocationSpec
-
-	body, err := ioutil.ReadAll(job.Body)
+func (p *ParsedJob) TempPath(name string) (string, error) {
+	tmp, err := p.EnsureTemp()
 	if err != nil {
-		return nil, fmt.Errorf("reading body: %w", err)
+		return "", err
 	}
+	return path.Join(tmp, name), nil
+}
 
-	if err := json.Unmarshal(body, &spec); err != nil {
-		return nil, fmt.Errorf("parsing body: %w", err)
+func runOne(ctx context.Context, store store.Store, job *http.Response) (interface{}, error) {
+	defer job.Body.Close()
+
+	parsed, err := parseJob(ctx, store, job.Body)
+	if err != nil {
+		return nil, err
 	}
+	defer parsed.Cleanup()
 
 	cmd := exec.Cmd{
-		Path: path.Join(root, handler),
-		Dir:  root,
-		Args: append([]string{handler}, spec.Args...),
+		Path: parsed.Exe,
+		Dir:  parsed.Root,
+		Args: parsed.Args,
 	}
-	if spec.Stdin != "" {
-		cmd.Stdin = strings.NewReader(spec.Stdin)
+	if parsed.Stdin != nil {
+		cmd.Stdin = bytes.NewReader(parsed.Stdin)
 	}
 	var stdout, stderr bytes.Buffer
 	cmd.Stderr = &stderr
@@ -101,11 +148,102 @@ func runOne(job *http.Response) (interface{}, error) {
 		return nil, fmt.Errorf("starting command: %w", err)
 	}
 	cmd.Wait()
-	resp := InvocationResponse{
+	resp := protocol.InvocationResponse{
 		ExitStatus: cmd.ProcessState.ExitCode(),
-		Stdout:     stdout.String(),
-		Stderr:     stderr.String(),
+	}
+
+	resp.Stdout, err = protocol.NewBlob(ctx, store, stdout.Bytes())
+	if err != nil {
+		resp.Stdout = &protocol.Blob{Err: err.Error()}
+	}
+	resp.Stderr, err = protocol.NewBlob(ctx, store, stderr.Bytes())
+	if err != nil {
+		resp.Stderr = &protocol.Blob{Err: err.Error()}
+	}
+	if parsed.Outputs != nil {
+		resp.Outputs = make(map[string]protocol.Blob, len(parsed.Outputs))
+	}
+	for out, path := range parsed.Outputs {
+		var blob *protocol.Blob
+		data, err := ioutil.ReadFile(path)
+		if err == nil {
+			blob, err = protocol.NewBlob(ctx, store, data)
+		}
+		if err != nil {
+			blob = &protocol.Blob{Err: err.Error()}
+		}
+		resp.Outputs[out] = *blob
 	}
 
 	return resp, nil
+}
+
+func parseJob(ctx context.Context, store store.Store, body io.ReadCloser) (*ParsedJob, error) {
+	handler := os.Getenv("_HANDLER")
+	root := os.Getenv("LAMBDA_TASK_ROOT")
+
+	data, err := ioutil.ReadAll(body)
+	if err != nil {
+		return nil, fmt.Errorf("reading body: %w", err)
+	}
+
+	var spec protocol.InvocationSpec
+	if err := json.Unmarshal(data, &spec); err != nil {
+		return nil, fmt.Errorf("parsing body: %w", err)
+	}
+
+	job := ParsedJob{
+		Exe:  path.Join(root, handler),
+		Root: root,
+		Args: []string{handler},
+	}
+
+	if spec.Stdin != nil {
+		job.Stdin, err = spec.Stdin.Read(ctx, store)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	for i, arg := range spec.Args {
+		var s string
+		if err := json.Unmarshal(arg, &s); err == nil {
+			job.Args = append(job.Args, s)
+			continue
+		}
+		var io protocol.Arg
+		if err := json.Unmarshal(arg, &io); err != nil {
+			return nil, fmt.Errorf("unable to interpret arg: %q", arg)
+		}
+
+		var argpath string
+
+		if io.In != nil {
+			argpath, err = job.TempPath(fmt.Sprintf("arg-%d", i))
+			if err != nil {
+				return nil, err
+			}
+			data, err := io.In.Read(ctx, store)
+			if err != nil {
+				return nil, err
+			}
+			if err := ioutil.WriteFile(argpath, data, 0600); err != nil {
+				return nil, err
+			}
+		}
+		if io.Out != nil {
+			if argpath == "" {
+				argpath, err = job.TempPath(fmt.Sprintf("out-%d", i))
+				if err != nil {
+					return nil, err
+				}
+			}
+			if job.Outputs == nil {
+				job.Outputs = make(map[string]string)
+			}
+			job.Outputs[*io.Out] = argpath
+		}
+		job.Args = append(job.Args, argpath)
+	}
+	return &job, nil
 }
---
+++ protocol/invocation.go
@@ -0,0 +1,64 @@
+package protocol
+
+import (
+	"context"
+	"encoding/base64"
+	"encoding/json"
+	"unicode/utf8"
+
+	"github.com/nelhage/llama/store"
+)
+
+const MaxInlineBlob = 10 * 1024
+
+type Blob struct {
+	String string `json:"s,omitempty"`
+	Bytes  []byte `json:"b,omitempty"`
+	Ref    string `json:"r,omitempty"`
+	Err    string `json:"e,omitempty"`
+}
+
+type Arg struct {
+	In  *Blob   `json:"i,omitempty"`
+	Out *string `json:"o,omitempty"`
+}
+
+type InvocationSpec struct {
+	Args  []json.RawMessage `json:"args"`
+	Stdin *Blob             `json:"stdin,omitempty"`
+}
+
+type InvocationResponse struct {
+	ExitStatus int             `json:"status"`
+	Stdout     *Blob           `json:"stdout,omitempty"`
+	Stderr     *Blob           `json:"stderr,omitempty"`
+	Outputs    map[string]Blob `json:"outputs,omitempty"`
+}
+
+func (b *Blob) Read(ctx context.Context, store store.Store) ([]byte, error) {
+	if b.String != "" {
+		return []byte(b.String), nil
+	}
+	if b.Bytes != nil {
+		return b.Bytes, nil
+	}
+	if b.Ref != "" {
+		return store.Get(ctx, b.Ref)
+	}
+	return nil, nil
+}
+
+func NewBlob(ctx context.Context, store store.Store, bytes []byte) (*Blob, error) {
+	stringOk := utf8.Valid(bytes)
+	if stringOk && len(bytes) < MaxInlineBlob {
+		return &Blob{String: string(bytes)}, nil
+	}
+	if base64.StdEncoding.EncodedLen(len(bytes)) < MaxInlineBlob {
+		return &Blob{Bytes: bytes}, nil
+	}
+	id, err := store.Store(ctx, bytes)
+	if err != nil {
+		return nil, err
+	}
+	return &Blob{Ref: id}, nil
+}
