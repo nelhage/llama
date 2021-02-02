8845cbdfa7cba71a1b3b9a363d49dde21df1cfee
Nelson Elhage <nelhage@nelhage.com>
2021-02-02 13:32:08 -0800

    Add a local cache to the s3 store

--- store/s3store/s3.go
+++ store/s3store/s3.go
@@ -22,6 +22,7 @@ import (
 	"io/ioutil"
 	"net/url"
 	"path"
+	"sync"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
@@ -40,6 +41,26 @@ type Store struct {
 	session *session.Session
 	s3      *s3.S3
 	url     *url.URL
+
+	cache cache
+}
+
+type cache struct {
+	sync.Mutex
+	seen map[string]struct{}
+}
+
+func (c *cache) hasObject(id string) bool {
+	c.Lock()
+	defer c.Unlock()
+	_, ok := c.seen[id]
+	return ok
+}
+
+func (c *cache) addObject(id string) {
+	c.Lock()
+	defer c.Unlock()
+	c.seen[id] = struct{}{}
 }
 
 func FromSession(s *session.Session, address string) (*Store, error) {
@@ -59,6 +80,9 @@ func FromSessionAndOptions(s *session.Session, address string, opts Options) (*S
 		session: s,
 		s3:      s3.New(s, aws.NewConfig().WithS3DisableContentMD5Validation(true)),
 		url:     u,
+		cache: cache{
+			seen: make(map[string]struct{}),
+		},
 	}, nil
 }
 
@@ -67,6 +91,11 @@ func (s *Store) Store(ctx context.Context, obj []byte) (string, error) {
 	defer span.End()
 	csum := blake2b.Sum256(obj)
 	id := hex.EncodeToString(csum[:])
+
+	if s.cache.hasObject(id) {
+		return id, nil
+	}
+
 	key := aws.String(path.Join(s.url.Path, id))
 	var err error
 
@@ -78,6 +107,7 @@ func (s *Store) Store(ctx context.Context, obj []byte) (string, error) {
 			Key:    key,
 		})
 		if err == nil {
+			s.cache.addObject(id)
 			span.AddField("s3.exists", true)
 			return id, nil
 		}
@@ -98,6 +128,7 @@ func (s *Store) Store(ctx context.Context, obj []byte) (string, error) {
 	if err != nil {
 		return "", err
 	}
+	s.cache.addObject(id)
 	return id, nil
 }
 
@@ -123,6 +154,7 @@ func (s *Store) Get(ctx context.Context, id string) ([]byte, error) {
 	if gotId != id {
 		return nil, fmt.Errorf("object store mismatch: got csum=%s expected %s", gotId, id)
 	}
+	s.cache.addObject(id)
 
 	span.AddField("s3.read_bytes", len(body))
 
