7796591b8af28096739b0f73989711219cf9f04f
Nelson Elhage <nelhage@nelhage.com>
2021-02-26 20:42:31 -0800

    llama store: accept multiple args

--- cmd/llama/objects.go
+++ cmd/llama/objects.go
@@ -42,18 +42,20 @@ func (c *StoreCommand) SetFlags(flags *flag.FlagSet) {
 func (c *StoreCommand) Execute(ctx context.Context, flag *flag.FlagSet, _ ...interface{}) subcommands.ExitStatus {
 	global := cli.MustState(ctx)
 
-	bytes, err := ioutil.ReadFile(flag.Arg(0))
-	if err != nil {
-		log.Printf("read %q: %v\n", flag.Arg(0), err)
-		return subcommands.ExitFailure
-	}
+	for _, arg := range flag.Args() {
+		bytes, err := ioutil.ReadFile(arg)
+		if err != nil {
+			log.Printf("read %q: %v\n", arg, err)
+			return subcommands.ExitFailure
+		}
 
-	id, err := global.MustStore().Store(ctx, bytes)
-	if err != nil {
-		log.Printf("storing %q: %v\n", flag.Arg(0), err)
-		return subcommands.ExitFailure
+		id, err := global.MustStore().Store(ctx, bytes)
+		if err != nil {
+			log.Printf("storing %q: %v\n", arg, err)
+			return subcommands.ExitFailure
+		}
+		log.Printf("object %q stored id=%s", arg, id)
 	}
-	log.Printf("object stored id=%s", id)
 
 	return subcommands.ExitSuccess
 }
